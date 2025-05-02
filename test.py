72a3bc3d3542e6478165e74a28beaad39c3d8d25
Caulhong <1900013087@pku.edu.cn>
2025-05-02 08:36:00 +0800

    lc

--- test.py
+++ test.py
@@ -1,2 +1,41 @@
-print("final")
-print("hh")
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        q = collections.deque()
+        q.append(root)
+        res = []
+        while q:
+            tmp = q.popleft()
+            if tmp:
+                res.append(q.val)
+                q.append(q.left)
+                q.append(q.right)
+            else:
+                res.append("null")
+        return ','.join(res)
+
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
